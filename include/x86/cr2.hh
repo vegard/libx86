c6c309334d023a67ab976377f6a9324b74932a82
Vegard Nossum <vegard.nossum@gmail.com>
2012-03-25 18:39:41 +0200

    Add x86::cr2 and x86_64::cr2
    There's nothing yet except static functions for reading and writing the
    register itself. We might add a method to return the actual address, but
    I'm not yet sure if we should return a uint32_t/uint64_t or a pointer.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

---
+++ include/x86/cr2.hh
@@ -0,0 +1,59 @@
+/*
+ * libx86
+ * Copyright (C) 2012  Vegard Nossum <vegardno@ifi.uio.no>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef X86_CR2_HH
+#define X86_CR2_HH
+
+#include <cassert>
+#include <cstdint>
+
+namespace x86 {
+
+class cr2 {
+public:
+	uint32_t value;
+
+	cr2(uint32_t value = 0):
+		value(value)
+	{
+	}
+
+	~cr2()
+	{
+	}
+
+	static inline cr2 read()
+	{
+		cr2 x;
+		asm volatile ("movl %%cr2, %0"
+			: "=r" (x.value));
+		return x;
+	}
+
+	static inline void write(cr2 x)
+	{
+		asm volatile ("movl %0, %%cr2"
+			:
+			: "r" (x.value));
+	}
+};
+
+}
+
+#endif
